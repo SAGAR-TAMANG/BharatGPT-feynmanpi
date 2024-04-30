1fdbaf0638e7b3d665837df3cdca0c0c887b04f0
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-04-30 19:57:16 +0530

    latest update

--- backend/main/api.py
+++ backend/main/api.py
@@ -1,4 +1,20 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from django.http import JsonResponse
 
 client = NinjaAPI()
 
+class ChatSchema(Schema):
+  id: str
+  message: str
+
+@client.get("/hello")
+def hello(request):
+  return JsonResponse({
+    'message': 'Hello World'
+  })
+
+@client.post("/chat", response=ChatSchema)
+def chat_gemini(request):
+  return JsonResponse({
+    'message': 'API Requst to the GPT sent.'
+  })
