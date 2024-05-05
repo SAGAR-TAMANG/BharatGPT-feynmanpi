e54afc34e0b820e386267a770fc27e40f83d3444
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-05-05 20:56:39 +0530

    removed databases/backend & added requirements.txt

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ChatMessage, ChatSession
+# from .models import ChatMessage, ChatSession
 
 # Register your models here.
-admin.site.register(ChatSession)
-admin.site.register(ChatMessage)
+# admin.site.register(ChatSession)
+# admin.site.register(ChatMessage)
--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-class ChatSession(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    session_id = models.CharField(max_length=100)
+# class ChatSession(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     session_id = models.CharField(max_length=100)
 
-class ChatMessage(models.Model):
-    session = models.ForeignKey(ChatSession, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+# class ChatMessage(models.Model):
+#     session = models.ForeignKey(ChatSession, on_delete=models.CASCADE)
+#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
+#     message = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
 
