eaf08563b7784f8b1a85852605e7edaaca2ad733
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-05-08 02:32:41 +0530

    latest

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, chat_blob, test
+from .views import index, chat_blob, assetlinks_json, test
 
 urlpatterns = [
   path('', index, name='index'),
   path('chat', chat_blob, name='chatBharatGPT'),
   path('test', test, name='test'),
+  path('.well-known/assetlinks.json', assetlinks_json, name='assetlinks_json'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -251,6 +251,16 @@ def bharatGPT(message, history=[]):
 def history_creator(history):
   print("\nHISTORY\n", history)
   
+import json
+from django.http import JsonResponse
+from django.conf import settings
+
+def assetlinks_json(request):
+    file_path = os.path.join(settings.STATIC_ROOT, '.well-known', 'assetlinks.json')
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return JsonResponse(data, safe=False)
+
 
 
 def test(request):
